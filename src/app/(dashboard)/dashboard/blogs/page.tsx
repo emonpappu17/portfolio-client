6110cecc34b5ff46d9a561e947c4149a1137bc76
emonpappu17 <emon.pappu.5@gmail.com>
2025-10-01 14:11:55 +0600

    change: dashboard navigation labout

--- src/app/(dashboard)/dashboard/blogs/page.tsx
+++ src/app/(dashboard)/dashboard/blogs/page.tsx
@@ -1,10 +1,19 @@
+import { Button } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
 import React from 'react';
 
 const DashboardBlogsPage = () => {
     return (
-        <div>
-            <h1>DashboardBlogsPage</h1>
-        </div>
+        <main>
+
+            <div className='flex justify-between items-center'>
+                <h1 className='text-2xl'>Manage Blogs</h1>
+                <Button>
+                    <Plus></Plus>
+                    Create blog
+                </Button>
+            </div>
+        </main>
     );
 };
 
--- src/components/headRightSideSec.tsx
+++ src/components/headRightSideSec.tsx
@@ -2,44 +2,26 @@
 "use client"
 
 import { getProfile } from '@/services/user/profile';
+import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
 import { ModeToggle } from './shared/ModeToggle';
 import { Button } from './ui/button';
-import { useQuery } from '@tanstack/react-query';
 
 export const RightHead = ({ isScrolled }: { isScrolled: boolean }) => {
-    // const profile = await getProfile();
-
-    // const [profile, setProfile] = useState<any>(null)
-    // console.log(profile);
-
-    // useEffect(() => {
-    //     getProfile().then(setProfile).catch(console.error)
-    // }, [])
 
     const { data, isLoading } = useQuery({
         queryKey: ['profile'],
         queryFn: getProfile,
         retry: false
     })
-    // console.log(isLoading);
-    // console.log("tack res", data);
 
     return (
         <div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
             <ModeToggle />
-            {/* <Button
-                asChild
-                variant="outline"
-                size="sm"
-                className={cn(isScrolled && "lg:hidden")}
-            >
-                <Link href="/login">Login</Link>
-            </Button> */}
-
             {data?.success && !isLoading ? (
-                <Button asChild variant="outline" size="sm">
+                <Button
+                    // className={cn(isScrolled && "lg:hidden")}
+                    asChild variant="outline" size="sm">
                     <Link href="/dashboard">Dashboard</Link>
                 </Button>
             ) : (
@@ -51,4 +33,3 @@ export const RightHead = ({ isScrolled }: { isScrolled: boolean }) => {
     );
 };
 
-// export default RightHead;
